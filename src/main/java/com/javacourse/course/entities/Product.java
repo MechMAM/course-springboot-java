e70e0362136de89940de5124e38acbdb0013298a
Miguel A. M <engmiguelmonteiro@gmail.com>
2022-08-05 23:03:07 -0300

    Product

--- src/main/java/com/javacourse/course/entities/Product.java
+++ src/main/java/com/javacourse/course/entities/Product.java
@@ -13,26 +13,31 @@ import javax.persistence.Table;
 import javax.persistence.Transient;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable{
+@Table(name = "tb_product")
+public class Product implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	private String description;
+	private double price;
+	private String imgUrl;
 	
 	@Transient //Impede o JPA de interpretar esse atributo
-	private Set<Product> products = new HashSet<>();
+	private Set<Category> categories = new HashSet<>();
 	
-	public Category() {
-		super();
+	public Product() {
 	}
-	
-	public Category(Long id, String name) {
+
+	public Product(Long id, String name, String description, double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
 	}
 
 	public Long getId() {
@@ -51,10 +56,38 @@ public class Category implements Serializable{
 		this.name = name;
 	}
 
-	public Set<Product> getProducts() {
-		return products;
+	public String getDescription() {
+		return description;
 	}
-	
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -68,13 +101,7 @@ public class Category implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
-	}
-
-
-
-	
-	
-
+	}	
 }
---
+++ src/main/java/com/javacourse/course/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.javacourse.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javacourse.course.entities.Product;
+import com.javacourse.course.repositories.ProductRepository;
+
+@Service //registra como componente do spring framework, pode ser usado Component, Service ou Repository
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
